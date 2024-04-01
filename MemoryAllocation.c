d9a193e44c0a2abee947225e879608cc3cfaad9b
AslamSuneer <103572084+AslamSuneer@users.noreply.github.com>
2024-04-01 22:32:49 +0530

    Update MemoryAllocation.c

--- MemoryAllocation.c
+++ MemoryAllocation.c
@@ -39,3 +39,47 @@ void main() {
             printf("Not Allocated\n");
     }
 }
+#include <stdio.h>
+
+void main() {
+    int fragment[20], b[20], p[20], barray[20], parray[20], i, j, nb, np, temp, lowest;
+
+    printf("\n\t\t\tMemory Management Scheme - Best Fit\n");
+    printf("Enter the number of blocks: ");
+    scanf("%d", &nb);
+    printf("Enter the size of the blocks:\n");
+    for (i = 1; i <= nb; i++) {
+        printf("Block no.%d: ", i);
+        scanf("%d", &b[i]);
+        barray[i] = 0; // Initialize block array
+    }
+
+    printf("\nEnter the number of processes: ");
+    scanf("%d", &np);
+    printf("Enter the size of the processes:\n");
+    for (i = 1; i <= np; i++) {
+        printf("Process no.%d: ", i);
+        scanf("%d", &p[i]);
+        parray[i] = 0; // Initialize process array
+    }
+
+    for (i = 1; i <= np; i++) {
+        lowest = 9999; // Initialize lowest to a large value
+        for (j = 1; j <= nb; j++) {
+            if (barray[j] == 0) { // Check if block is not allocated
+                temp = b[j] - p[i];
+                if (temp >= 0 && temp < lowest) {
+                    parray[i] = j; // Allocate block to process
+                    lowest = temp; // Update lowest fragment
+                }
+            }
+        }
+        fragment[i] = lowest;
+        barray[parray[i]] = 1; // Mark block as allocated
+    }
+
+    printf("\nProcess_no\tProcess_size\tBlock_no\tBlock_size\tFragment\n");
+    for (i = 1; i <= np && parray[i] != 0; i++) {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i, p[i], parray[i], b[parray[i]], fragment[i]);
+    }
+}
